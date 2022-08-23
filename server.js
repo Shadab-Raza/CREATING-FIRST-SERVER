7cdecfe8c13c13003c3f3047e2cbf2113facf40d
shadab1928raza <shadab.razamd@gmail.com>
2022-08-23 12:17:44 +0530

    routes created

--- server.js
+++ server.js
@@ -2,11 +2,31 @@ const http = require('http');
 
 const port = 3000;
 
+const toDoList = ["Complete Node Bytes", "Play Cricket"];
 
-http.createServer((request, response) =>{
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<h1>Hello, this is from my server</h1>");
-    response.end();
+http.createServer((req, res) =>{
+    const {method, url} = req;
+    
+    if(url === "/todos"){
+        if(method === "GET"){
+            res.writeHead(200, {"Content-Type" : "text/html"});
+            res.write(toDoList.toString());
+        } else if(method === 'POST'){
+            let body = "";
+            req.on('error',() => {
+                console.error(err)
+            }).on('data',(chunk) => {
+                body += chunk;
+            }).on('end', () => {
+                body = JSON.parse(body);
+                console.log("data: ", body);
+            });
+        }
+        else{
+            res.writeHead(404);
+        }
+    }
+    res.end();
 })
 .listen(port, () => {
     console.log(`Node.js server started on port ${port}`);
